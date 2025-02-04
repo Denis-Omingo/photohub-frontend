805799715eeb87da2ec2b5603bf03711c977762c
Denis-Omingo <omingodenis7@gmail.com>
2025-02-04 12:48:25 +0300

    feat(frontend):initialize react project with vite, tailwind css, typescript, and shadcn

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+  <div className="bg-slate-700 p-20">
+    <Button>Click Me</Button>
+  </div>
+  )
+}
+
+export default App

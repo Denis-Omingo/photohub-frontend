997f4b7d4a2d08bdc623f15a43820783925e97f5
Denis-Omingo <omingodenis7@gmail.com>
2025-02-05 11:05:51 +0300

    feat(Main Nav): Set up the main nav only visible from medium screens onwards

---
+++ src/components/MainNav.tsx
@@ -0,0 +1,10 @@
+import { LogIn } from "lucide-react";
+import { Button } from "./ui/button";
+
+const MainNav =()=>{
+    return(
+       <LogIn className="text-primary-foreground"/>
+    )
+}
+
+export default MainNav;

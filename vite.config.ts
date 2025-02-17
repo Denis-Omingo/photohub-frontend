904f73b6cd0fbab92b4a58b2956fc77073fb8656
Denis-Omingo <omingodenis7@gmail.com>
2025-02-17 17:51:11 +0300

    Add build configurations for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, 
+  },
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"), 
